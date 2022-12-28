c97e99abd1ae4d77899202dbcf6e61f8d772b3cc
Kaleb Berry <kaleb.berry@outlook.com>
2022-12-27 19:39:01 -0600

    updating to astro.js

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), mdx()],
+  output: "server",
+  adapter: vercel()
+});
