ae11528fc3d619b07459a3af55d9ec241ae2db48
Kaleb Berry <kaleb.berry@outlook.com>
2023-10-24 19:28:16 -0500

    testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,9 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx()],
-  output: "server",
-  adapter: vercel()
 });
