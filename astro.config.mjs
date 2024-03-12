186a6d70a1b6619d72cdfd358a239d5d9db183a1
Kaleb Berry <kaleb.berry@outlook.com>
2024-03-12 18:20:53 -0500

    adding

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from "astro/config";
-// import swup from '@swup/astro';
-
-// https://astro.build/config
 import react from "@astrojs/react";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx()],
-  // swup()
+
 });
