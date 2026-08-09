29c6818f0e03bc8b70d2eb70c09f729ca6d0002f
Kaleb Berry <kaleb.berry@outlook.com>
2026-08-08 22:40:42 -0500

    more optimization cleaning up files

--- src/utils/currency.ts
+++
@@ -1,3 +0,0 @@
-export const formatCurrency = (value: number): string => {
-	return `$${value.toFixed(2)}`;
-};
--- src/utils/formatDate.ts
+++
@@ -1,3 +0,0 @@
-export const formatDate = (date: string): string => {
-	return new Date(date).toLocaleDateString();
-};
