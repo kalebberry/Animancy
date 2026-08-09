821a0c831aa24ba183ed962838d87c13ca9f4798
Kaleb Berry <kaleb.berry@outlook.com>
2026-08-08 22:17:54 -0500

    adding more optimizations to css website stuff

--- src/components/vue/dashboard/SafeToSpend.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-	<div class="safe-to-spend">
-		<h2>Studio Eien Web Starter Template</h2>
-		<p>Let's get started!</p>
-	</div>
-</template>
--- src/stores/useBudgetStore.ts
+++
@@ -1,7 +0,0 @@
-import { defineStore } from 'pinia';
-
-export const useBudgetStore = defineStore('budget', {
-	state: () => ({
-		safeToSpend: 1250,
-	}),
-});
